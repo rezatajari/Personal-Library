9dd4025c2ed07e725e53f641158d06c25385f28c
rezatajari <reza.tajari70@gmail.com>
2024-11-24 13:43:05 +0330

    BookRepo

---
+++ BookRepository.cs
@@ -0,0 +1,151 @@
+﻿using Personal_Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Personal_Library.LibraryManagement;
+
+namespace Personal_Library
+{
+    internal class BookRepository
+    {
+        private LibraryManagement _library;
+        public BookRepository()
+        {
+            _library = new LibraryManagement();
+        }
+
+        public void AddBook()
+        {
+            try
+            {
+                Books newBook = new Books();
+
+                Console.WriteLine("Please enter your title book:");
+                newBook.Title = Console.ReadLine();
+
+                Console.WriteLine("Please enter author of the book:");
+                newBook.Author = Console.ReadLine();
+
+                Console.WriteLine("Enter a genre {1. Fantasy,2. ScienceFiction,3. Biography}");
+                string input = Console.ReadLine();
+                bool isValidGenre = ValidationCenter.GetValidGenre(input);
+                Enum.TryParse(input, true, out Books.GenreType genre);
+                newBook.Genre = genre;
+
+                _library.AddBook(newBook);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"The {newBook.Title} is Added!");
+                Console.WriteLine("Please enter a key");
+                Console.ReadKey();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"There is a error: {err.Message}");
+            }
+        }
+
+        public void RemoveBook()
+        {
+            try
+            {
+                List<Books> listBooks = _library.ListBook();
+
+                Console.WriteLine("Please enter remove your title book?");
+                string bookTitle = Console.ReadLine();
+
+                Books removeBook = _library.SearchBook(bookTitle);
+
+                if (ValidationCenter.IsBookValid(removeBook))
+                {
+                    _library.RemoveBook(removeBook);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Removed. Press any key to continue...");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"There is a error: {err.Message}");
+            }
+        }
+
+        public void SearchBook()
+        {
+            bool searchFlag = false;
+
+            while (!searchFlag)
+            {
+                try
+                {
+                    Console.WriteLine("Enter your book title do you want it?");
+                    string bookTitle = Console.ReadLine();
+
+                    var searchBook = _library.SearchBook(bookTitle);
+
+                    if (!ValidationCenter.IsBookValid(searchBook))
+                    {
+                        Console.WriteLine("Try again choose an option:\n1. search\n2. Go to menu\n3. Exit");
+
+                        string inputOption = Console.ReadLine();
+                        bool isValid = ValidationCenter.IsInputValid(inputOption);
+                        Enum.TryParse(inputOption, true, out MySearchOption selectSearchOption);
+
+                        switch (selectSearchOption)
+                        {
+                            case MySearchOption.SearchAgain:
+                                break;
+                            case MySearchOption.GoToMenu:
+                                searchFlag = true;
+                                break;
+                            case MySearchOption.Exit:
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                Console.WriteLine("Invalid option, please try again.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your title book is {searchBook.Title} and your author book is {searchBook.Author} and genre book is {searchBook.Genre} book");
+                    }
+                }
+                catch (Exception err)
+                {
+
+                    Console.WriteLine($"There is a error: {err.Message}");
+                }
+            }
+        }
+
+        public void ListBook()
+        {
+            _library.ListBook();
+            ReadyToOrder();
+        }
+
+
+        void ReadyToOrder()
+        {
+            try
+            {
+                Console.WriteLine("1. Go to menu\n2. Exit");
+
+                int inputUser = Convert.ToInt16(Console.ReadLine());
+                if (inputUser == 2)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"There is a error: {err.Message}");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,142 +1,29 @@
 ﻿using Personal_Library;
 using Personal_Library.Entities;
 using static Personal_Library.LibraryManagement;
-using static Personal_Library.ValidationCenter;
 
 bool isValidMenu;
-LibraryManagement library = new LibraryManagement();
+BookRepository bookRepo = new BookRepository();
 bool exitApp = false;
 
 while (!exitApp)
 {
-    Console.Clear();
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("Welcome to the Personal Library Management System");
-    Console.WriteLine("1. Add a Book\n2. Remove a Book\n3. Search for a Book\n4. List All Books\n5. Exit\r\nEnter your choice:");
+    Welcome();
+    MainMenu selectMenu = SelectMenu();
 
-    string selectMenu = Console.ReadLine();
-    isValidMenu = ValidationCenter.IsValidInput(selectMenu);
-    Enum.TryParse(selectMenu, true, out MainMenu selectOption);
-
-    switch (selectOption)
+    switch (selectMenu)
     {
         case MainMenu.Add:
-            try
-            {
-                Books newBook = new Books();
-
-                Console.WriteLine("Please enter your title book:");
-                newBook.Title = Console.ReadLine();
-
-                Console.WriteLine("Please enter author of the book:");
-                newBook.Author = Console.ReadLine();
-
-                Console.WriteLine("Enter a genre {1. Fantasy,2. ScienceFiction,3. Biography}");
-                string input = Console.ReadLine();
-                bool isValidGenre = ValidationCenter.GetValidGenre(input);
-                Enum.TryParse(input, true, out Books.GenreType genre);
-                newBook.Genre = genre;
-
-                library.AddBook(newBook);
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"The {newBook.Title} is Added!");
-                Console.WriteLine("Please enter a key");
-                Console.ReadKey();
-            }
-            catch (Exception err)
-            {
-
-                Console.WriteLine($"There is a error: {err.Message}");
-            }
-
+            bookRepo.AddBook();
             break;
         case MainMenu.Remove:
-            try
-            {
-                List<Books> listBooks = library.ListBook();
-
-                Console.WriteLine("Please enter remove your title book?");
-                string bookTitle = Console.ReadLine();
-
-                Books removeBook = library.SearchBook(bookTitle);
-
-                if (ValidationCenter.IsBookValid(removeBook))
-                {
-                    library.RemoveBook(removeBook);
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Removed. Press any key to continue...");
-                    Console.ResetColor();
-                    Console.ReadKey();
-                }
-            }
-            catch (Exception err)
-            {
-                Console.WriteLine($"There is a error: {err.Message}");
-            }
+            bookRepo.RemoveBook();
             break;
         case MainMenu.Search:
-            bool searchFlag = false;
-            while (!searchFlag)
-            {
-                try
-                {
-                    Console.WriteLine("Enter your book title do you want it?");
-                    string bookTitle = Console.ReadLine();
-
-                    var searchBook = library.SearchBook(bookTitle);
-
-                    if (!ValidationCenter.IsBookValid(searchBook))
-                    {
-                        Console.WriteLine("Try again choose an option:\n1. search\n2. Go to menu\n3. Exit");
-
-                        string inputOption = Console.ReadLine();
-                        bool isValid = ValidationCenter.IsInputValid(inputOption);
-                        Enum.TryParse(inputOption, true, out MySearchOption selectSearchOption);
-
-                        switch (selectSearchOption)
-                        {
-                            case MySearchOption.SearchAgain:
-                                break;
-                            case MySearchOption.GoToMenu:
-                                searchFlag = true;
-                                break;
-                            case MySearchOption.Exit:
-                                Environment.Exit(0);
-                                break;
-                            default:
-                                Console.WriteLine("Invalid option, please try again.");
-                                break;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Your title book is {searchBook.Title} and your author book is {searchBook.Author} and genre book is {searchBook.Genre} book");
-                    }
-                }
-                catch (Exception err)
-                {
-
-                    Console.WriteLine($"There is a error: {err.Message}");
-                }
-            }
+            bookRepo.SearchBook();
             break;
         case MainMenu.List:
-            library.ListBook();
-            try
-            {
-                Console.WriteLine("1. Go to menu\n2. Exit");
-
-                int inputUser = Convert.ToInt16(Console.ReadLine());
-                if (inputUser == 2)
-                {
-                    Environment.Exit(0);
-                }
-            }
-            catch (Exception err)
-            {
-                Console.WriteLine($"There is a error: {err.Message}");
-            }
+            bookRepo.ListBook();
             break;
         case MainMenu.Exit:
             exitApp = true;
@@ -145,8 +32,21 @@ while (!exitApp)
             Console.WriteLine("Please only enter number between 1 to 5");
             break;
     }
+}
 
-
+void Welcome()
+{
+    Console.Clear();
+    Console.ResetColor();
+    Console.WriteLine("Welcome to the Personal Library Management System");
+}
+MainMenu SelectMenu()
+{
+    Console.WriteLine("1. Add a Book\n2. Remove a Book\n3. Search for a Book\n4. List All Books\n5. Exit\r\nEnter your choice:");
+    string input = Console.ReadLine();
+    isValidMenu = ValidationCenter.IsValidInput(input);
+    Enum.TryParse(input, true, out MainMenu selectMenu);
+    return selectMenu;
 }
 public enum MainMenu
 {
@@ -158,3 +58,10 @@ public enum MainMenu
 }
 
 
+//TODO: What’s Important for the Next Section:
+//      As you move forward, you might consider the following topics to enhance your library system:
+//1.    Persistence: Saving the books to a file or database (e.g., SQLite or MySQL).
+//2.	Advanced Validation: Handling more complex validations, such as ensuring that book titles are unique.
+//3.	User Interface: Moving from the console interface to a more user-friendly GUI, possibly using WPF (Windows Presentation Foundation) or ASP.NET Core for web-based applications.
+//4.	Error Handling: Ensure your application handles different types of errors (e.g., invalid input, file read/write errors) gracefully.
+//5.	Unit Testing: Implement unit tests for your methods to ensure they behave as expected.
